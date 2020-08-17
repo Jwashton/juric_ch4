3da661e6be6b59c817ae26b2642549c71c0a651e
William Ashton <William@AshtonFam.org>
2020-08-17 14:01:30 -0400

    End of Chapter 4

--- lib/todo_list.ex
+++ lib/todo_list.ex
@@ -45,3 +45,21 @@ defmodule TodoList do
     %TodoList{todo_list | entries: Map.delete(todo_list.entries, entry_id)}
   end
 end
+
+defimpl String.Chars, for: TodoList do
+  def to_string(_) do
+    "#TodoList"
+  end
+end
+
+defimpl Collectable, for: TodoList do
+  def into(original) do
+    {original, &into_callback/2}
+  end
+
+  defp into_callback(todo_list, {:cont, entry}) do
+    TodoList.add_entry(todo_list, entry)
+  end
+  defp into_callback(todo_list, :done), do: todo_list
+  defp into_callback(_todo_list, :halt), do: :ok
+end
